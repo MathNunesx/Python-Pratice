1658975dd109ad48790b6e35377275c940d59ab0
Matheus Nunes <matheusjobs04@gmail.com>
2025-03-04 16:00:29 -0300

    Calculadora

--- ex01/ex01.py
+++ ex01/ex01.py
@@ -1,3 +1,11 @@
 
-name = input("Digite o seu nome: ")
-print("Bom dia, " + name)
+print("Bem vindo a sua calculadora!")
+n1 = float(input("Digite o pirmeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+
+soma = n1 + n2
+subtração = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+print(f"Resultado das operações: Soma = {soma}, Subtração = {subtração}, Multiplicação = {multiplicacao}, Divisão: {divisao}" )
