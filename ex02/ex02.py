08fcee84dafa1acf85771a7c395fc5032f33a1bc
Matheus Nunes <matheusjobs04@gmail.com>
2025-03-04 16:06:33 -0300

    calculo velocidade media

---
+++ ex02/ex02.py
@@ -0,0 +1,9 @@
+print("Bem vindo!!")
+
+distancia = float(input("Digite a distância percorrida em KM: "))
+tempo = float(input("Digite o tempo de viagem: "))
+
+velocidade_media = distancia / tempo
+
+print(f"O Resultado é: {velocidade_media:.2f}Km/h")
+
